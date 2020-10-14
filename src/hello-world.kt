ca95ed6f745db9b9b71a83e4fe3575387b841fcd
yoon4027 <yooooonus@gmail.com>
2020-10-14 11:31:59 +0500

    bored �

---
+++ src/hello-world.kt
@@ -0,0 +1,8 @@
+fun helloworld(h: String) {
+   return println(h.toUpperCase())
+}
+fun main() {
+    var a = "Hello World.";
+    println(a.toUpperCase());
+    helloworld("this is the function.");
+}
